f9e3494153b7e942a1043655972dee05fa5725a2
mauriziopatino <mauriziopatino@hotmail.com>
2023-04-26 15:40:03 -0600

    Routes for controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\FriendController;
+use App\Http\Controllers\GenderController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\NotificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'users' => UserController::class,
+    'comments' => CommentController::class,
+    'friends' => FriendController::class,
+    'genders' => GenderController::class,
+    'notifications' => NotificationController::class,
+    'notifications_types' => NotificationsTypeController::class,
+    'permissions' => PermissionController::class,
+    'posts' => PostController::class,
+    'reactions' => ReactionController::class,
+    'roles' => RoleController::class,
+    'roles_permissions' => RolesPermissionController::class,
+]);
