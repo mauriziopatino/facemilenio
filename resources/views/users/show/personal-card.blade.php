cba3fc16cf4666ece07e50dbd898a132974c3c8a
mauriziopatino <mauriziopatino@hotmail.com>
2023-04-29 09:03:21 -0600

    Profile information card profile picture default

--- resources/views/users/show/personal-card.blade.php
+++ resources/views/users/show/personal-card.blade.php
@@ -1,6 +1,6 @@
 <div class="card mb-4">
     <div class="card-body text-center">
-        <img src="{{url('/images/profile_test.jpeg')}}" alt="avatar"
+        <img src="{{$friend->user->url_photo ?? url('/images/default_profile_picture.jpg')}}" alt="avatar"
         class="rounded-circle img-fluid" id="profile-picture">
         <h5 class="my-3">{{$user->name}} {{$user->last_name}}</h5>
         <p class="text-muted mb-1">{{($user->biography) ?? "No biography."}}</p>
